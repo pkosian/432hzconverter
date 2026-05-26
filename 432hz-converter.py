ef942605d1a75a1b23b154c40e14dd0605cefdac
pkosian <philipp.kosian@outlook.com>
2026-05-26 17:55:06 +0200

    Added support for settings file

---
+++ 432hz-converter.py
@@ -0,0 +1,53 @@
+import os, shutil, threading, pydub, mutagen, time, settings
+
+def speed_change(sound, speed=1.0):
+    sound_with_altered_frame_rate = sound._spawn(sound.raw_data, overrides={
+        "frame_rate": int(sound.frame_rate * speed)
+    })
+    return sound_with_altered_frame_rate.set_frame_rate(sound.frame_rate)
+
+def slowDownFile(f):
+    rating = 0
+    f_path = f.path
+    f_name = f.name
+    tag_file = mutagen.File(f_path)
+    try:
+        rating = int(tag_file['TXXX:POPM'].text[0])
+    except:
+        pass
+    if rating >= settings.min_rating:
+        if settings.just_copy in f_name:
+            shutil.copy(f_path, settings.output_path)
+            print("Copied: ", f_name)
+        elif settings.overwrite or not f_name[:-4] + settings.append + settings.filetype in existing_file_names:
+            print("Converting: ", f_name)
+            af = pydub.AudioSegment.from_file(f_path)
+            af_new = speed_change(af, settings.vel)
+            new_path = settings.output_path + f_name[:-4] + settings.append + settings.filetype
+            af_new.export(new_path, format = 'mp3', parameters=["-q:a", "0"])
+            new_tag_file = mutagen.File(new_path)
+            new_tag_file = tag_file
+            new_tag_file.save(new_path)
+            print("Finished: ", f_name)
+        else:
+            print("File already exists: " + f_name)
+    else:
+        print("Rating not high enough: ", f_name)
+
+if __name__ == "__main__":
+    threads = []
+    d = os.scandir(settings.input_path)
+    try:
+        os.mkdir(settings.output_path)
+    except:
+        pass
+    existing_file_names = [n.name for n in os.scandir(settings.output_path)]
+    for f in d:
+        t = threading.Thread(target=slowDownFile, args=(f,))
+        threads.append(t)
+    for t in threads:
+        while len([t for t in threads if t.is_alive()]) == settings.maxthreads:
+            time.sleep(0.05)
+        t.start()
+    for t in threads:
+        t.join()
